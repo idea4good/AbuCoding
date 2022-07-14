e6f51387be3d838bdf814b154c65b2c55fd3925b
idea4good <idea4good@outlook.com>
2022-07-14 11:00:01 +0800

    Create syscall.s

---
+++ source/syscall.s
@@ -0,0 +1,19 @@
+# 1) apt install nasm
+# 2) nasm -f elf64 syscall.s  && ld syscall.o
+# 3) ./a.out
+
+section .text
+  global _start
+    _start:
+      mov rdi, 1                    ;arg0
+      mov rdx, 8                    ;arg2
+      mov rbx, 0x0a646c726f576948   ;arg1 = HiWorld\n
+      ;mov rbx, 0x0a83989ff03a6948  ;arg1 = Hi:😃\n
+      push rbx
+      mov rsi, rsp
+      mov rax, 1
+      syscall
+
+      mov al, 60                  ;exit
+      syscall
+      
