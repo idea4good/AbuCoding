5ef5086225240d2ebe211ba3d22cf35b85b97ee8
idea4good <idea4good@outlook.com>
2025-10-18 01:33:50 +0800

    support read/write phy address for windbgext

--- source/windbgExt.cpp
+++ source/windbgExt.cpp
@@ -1,6 +1,9 @@
 // cl /LD windbgExt.cpp /link
 // .load windbgExt.dll
 // !hello
+// !readphys 0x18830000
+// !writephys 0x18830000 1
+// .unload windbgExt.dll
 
 #include <windows.h>
 #include <wdbgexts.h>
@@ -28,18 +31,59 @@ VOID hello()
 }
 
 extern "C" __declspec(dllexport)
-VOID WDBGAPI readphys(PCHAR args)
-{
-    ULONG64 physAddr = 0x18830000;
+VOID WDBGAPI readphys(PVOID hCurrentProcess, PVOID hCurrentThread, ULONG dwCurrentPc, ULONG dwProcessor, PCSTR args)
+{    
+    if (!args || !args)
+    {
+        dprintf("Usage: !readphys address\n");
+    }
+
+    ULONG64 address = 0;
+    if (sscanf_s(args, "%llx", &address) != 1)
+    {
+        dprintf("Invalid input. Please provide a hex address, e.g. !readphys 0x1234\n");
+        return;
+    }
 
     BYTE buffer[8] = {0};
     ULONG bytesRead = 0;
+    ReadPhysical(address, buffer, sizeof(buffer), &bytesRead);
 
-    ReadPhysical(physAddr, buffer, sizeof(buffer), &bytesRead);
-
-    dprintf("Read %lu bytes from physical address 0x%I64X:\n", bytesRead, physAddr);
-    for (ULONG i = 0; i < bytesRead; ++i) {
-        dprintf("%02X ", buffer[i]);
+    if (bytesRead > 0)
+    {
+        dprintf("Read %lu bytes from physical address 0x%I64X:\n", bytesRead, address);
+        for (ULONG i = 0; i < bytesRead; ++i)
+        {
+            dprintf("%02X ", buffer[i]);
+        }
+        dprintf("\n");
     }
-    dprintf("\n");
+    else
+    {
+        dprintf("Failed to read to physical address 0x%llx\n", address);
+    }
+}
+
+extern "C" __declspec(dllexport)
+VOID WDBGAPI writephys(PVOID hCurrentProcess, PVOID hCurrentThread, ULONG dwCurrentPc, ULONG dwProcessor, PCSTR args)
+{
+    ULONG64 address = 0;
+    ULONG value = 0;
+
+    if (!args || sscanf_s(args, "%llx %x", &address, &value) != 2) {
+        dprintf("Usage: !writephys <hexaddr> <hexvalue>\n");
+        return;
+    }
+
+    ULONG bytesWritten = 0;
+    WritePhysical(address, &value, sizeof(value), &bytesWritten);
+
+    if (bytesWritten > 0)
+    {
+        dprintf("Wrote %lu bytes to physical address 0x%llx\n", bytesWritten, address);
+    }
+    else
+    {
+        dprintf("Failed to write to physical address 0x%llx\n", address);
+    }
 }
